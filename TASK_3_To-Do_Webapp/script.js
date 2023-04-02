19d012cdb056d99c108dd879196ae789f952b4bb
Mayur Sonare <118804110+Mayur-666@users.noreply.github.com>
2023-04-02 10:00:13 +0530

    Add files via upload

---
+++ TASK_3_To-Do_Webapp/script.js
@@ -0,0 +1,104 @@
+const taskInput = document.querySelector(".task-input input");
+const filters = document.querySelectorAll(".filters span");
+const clearAll = document.querySelector(".clear-btn");
+const taskBox = document.querySelector(".task-box");
+
+let editId,
+isEditTask = false,
+todo = JSON.parse(localStorage.getItem("todo-list"));
+
+filters.forEach(btn => {
+    btn.addEventListener("click", () => {
+        document.querySelector("span.active").classList.remove("active");
+        btn.classList.add("active");
+        showTodo(btn.id);
+    });});
+
+function showTodo(filter) {
+    let liTag = "";
+    if(todo) {
+        todo.forEach((todo, id) => {
+            let completed = todo.status == "completed" ? "checked" : "";
+            if(filter == todo.status || filter == "all") {
+                liTag += `<li class="task">
+                            <label for="${id}">
+                                <input onclick="updateStatus(this)" type="checkbox" id="${id}" ${completed}>
+                                <p class="${completed}">${todo.name}</p>
+                            </label>
+                            <div class="settings">
+                                <i onclick="showMenu(this)" class="uil uil-ellipsis-h"></i>
+                                <ul class="task-menu">
+                                    <li onclick='editTask(${id}, "${todo.name}")'><i class="uil uil-pen"></i>Edit</li>
+                                    <li onclick='deleteTask(${id}, "${filter}")'><i class="uil uil-trash"></i>Delete</li>
+                                </ul>
+                            </div>
+                        </li>`;
+            }}); }
+
+    taskBox.innerHTML = liTag || `<p>You don't have any task here</p>`;
+    let checkTask = taskBox.querySelectorAll(".task");
+    !checkTask.length ? clearAll.classList.remove("active") : clearAll.classList.add("active");
+    taskBox.offsetHeight >= 300 ? taskBox.classList.add("overflow") : taskBox.classList.remove("overflow");
+}
+showTodo("all");
+
+function showMenu(selectedTask) {
+    let menuDiv = selectedTask.parentElement.lastElementChild;
+    menuDiv.classList.add("show");
+    document.addEventListener("click", e => {
+        if(e.target.tagName != "I" || e.target != selectedTask) {
+            menuDiv.classList.remove("show");
+        }
+    });
+}
+
+function updateStatus(selectedTask) {
+    let taskName = selectedTask.parentElement.lastElementChild;
+    if(selectedTask.checked) {
+        taskName.classList.add("checked");
+        todo[selectedTask.id].status = "completed";
+    } else {
+        taskName.classList.remove("checked");
+        todo[selectedTask.id].status = "pending";
+    }
+    localStorage.setItem("todo-list", JSON.stringify(todo))
+}
+
+function editTask(taskId, textName) {
+    editId = taskId;
+    isEditTask = true;
+    taskInput.value = textName;
+    taskInput.focus();
+    taskInput.classList.add("active");
+}
+
+function deleteTask(deleteId, filter) {
+    isEditTask = false;
+    todo.splice(deleteId, 1);
+    localStorage.setItem("todo-list", JSON.stringify(todo));
+    showTodo(filter);
+}
+
+clearAll.addEventListener("click", () => {
+    isEditTask = false;
+    todo.splice(0, todo.length);
+    localStorage.setItem("todo-list", JSON.stringify(todo));
+    showTodo()
+});
+
+taskInput.addEventListener("keyup", e => {
+    let userTask = taskInput.value.trim();
+    if(e.key == "Enter" && userTask) {
+        if(!isEditTask) {
+            todo = !todo ? [] : todo;
+            let taskInfo = {name: userTask, status: "pending"};
+            todo.push(taskInfo);
+        } else {
+            isEditTask = false;
+            todo[editId].name = userTask;
+        }
+        taskInput.value = "";
+        localStorage.setItem("todo-list", JSON.stringify(todo));
+        showTodo(document.querySelector("span.active").id);
+    }
+});
